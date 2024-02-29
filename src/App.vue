bdb69018208294ce393a7fe215d8c1d20b4299c4
Luca <lucafagiolini@live.it>
2024-02-29 15:47:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+
+
+
+
+<template></template>
+
+
+<style>
+/* #id {
+  display: flex;
+  margin: 0 auto;
+  max-width: 800px;
+} */
+</style>
